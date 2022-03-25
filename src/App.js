873700fc897b2456e6374436f353b6765d1f2d11
Chris <chrishughallen@gmail.com>
2022-03-25 13:22:28 +0100

    built todo list for deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {setState, useState, useEffect} from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([{id: Math.floor(Math.random()*1000), value: "Add your first Todo!", completed: false}])
+  const [newItem, setNewItem] = useState("")
+  let outstandingTasks = todoList.filter((item) => item.completed == false)
+  let completedTasks = todoList.filter((item) => item.completed)
+  const [outstandingCount, setOutstandingCount] = useState(todoList.filter((item) => item.completed))
+
+  const addItem = () => {
+    if(!newItem) {
+      return alert("Can\'t save an empty item!")
+    }
+    setTodoList((prevList) => [...prevList, {id: Math.floor(Math.random() * 1000),value: newItem, completed: false}])
+
+    setNewItem("")
+  }
+
+  const deleteItem = (id) => {
+    setTodoList((prevList) => prevList.filter((item) => item.id != id))
+  }
+
+  const toggleItem = (id) => {
+    setTodoList((prevList) => {
+      return prevList.map((item) => {
+       return item.id == id ? {...item, completed: !item.completed} : item
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="App">
+        {todoList.length == 0 && <h1>0 / 0</h1>}
+        {todoList.length >= 1 && <h1> {completedTasks.length} / {todoList.length}</h1>}
+          <div className="todo-input">
+            <input
+              autoComplete="off"
+              placeholder="new todo"
+              type="text"
+              name="todo"
+              onChange={(e) => setNewItem(e.target.value)}
+              value={newItem} 
+            />
+            <button onClick={addItem}>+</button>
+          </div>
+          
+        <ul>
+          {todoList.map((todo) => {
+            let css = todo.completed ? "completed-todo" : "uncompleted-todo"
+            return (
+              <li className={css} key={todo.id}>
+                <span onClick={() => toggleItem(todo.id)}>{todo.value}</span>
+                <button className="delete-button" onClick={() => deleteItem(todo.id)}>-</button>
+              </li>
+            )
+        })}
+        </ul>
+      </div>
     </div>
   );
 }
